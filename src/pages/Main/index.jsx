caa9ab83311c11c3b2dbbd64f983fa77244da399
Vadim Tarasenko <vadym.tarasenko99@gmail.com>
2019-09-28 20:59:33 +0300

    update

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,39 +2,8 @@ import React from 'react';
 
 import { ContentWrapper, Slider, MovieCard, Pagination } from 'components/reusable';
 
-// const movies = [{
-//   img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/Once_Upon_a_Time_in_Hollywood.jpg/269px-Once_Upon_a_Time_in_Hollywood.jpg',
-//   name: 'Однажди в голливуде',
-//   year: '2019',
-//   country: 'США',
-//   genre: 'Драма'
-// }, {
-//   img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/Once_Upon_a_Time_in_Hollywood.jpg/269px-Once_Upon_a_Time_in_Hollywood.jpg',
-//   name: 'Однажди в голливуде',
-//   year: '2019',
-//   country: 'США',
-//   genre: 'Драма'
-// }, {
-//   img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/Once_Upon_a_Time_in_Hollywood.jpg/269px-Once_Upon_a_Time_in_Hollywood.jpg',
-//   name: 'Однажди в голливуде',
-//   year: '2019',
-//   country: 'США',
-//   genre: 'Драма'
-// }, {
-//   img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/Once_Upon_a_Time_in_Hollywood.jpg/269px-Once_Upon_a_Time_in_Hollywood.jpg',
-//   name: 'Однажди в голливуде',
-//   year: '2019',
-//   country: 'США',
-//   genre: 'Драма'
-// }, {
-//   img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/Once_Upon_a_Time_in_Hollywood.jpg/269px-Once_Upon_a_Time_in_Hollywood.jpg',
-//   name: 'Однажди в голливуде',
-//   year: '2019',
-//   country: 'США',
-//   genre: 'Драма'
-// }];
-
 function Main({ movies }) {
+  
   movies.length = 30;
 
   return (
